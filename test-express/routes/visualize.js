8c2547c543f4c3aa0128fb5a10382eba2109c01f
Sk Arif <skarif2@gmail.com>
2019-11-01 03:41:16 +0600

    added express app

---
+++ test-express/routes/visualize.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/:id', function (req, res, next) {
+  res.render('visualize', {
+    title: 'Visualize',
+    roomId: req.params.id
+  });
+});
+
+module.exports = router;
